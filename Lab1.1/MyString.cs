b14991bf4959aaec046a2788c418789dd21083d1
Danil Fedorenko <danilfedorenko25@gmail.com>
2023-02-20 23:00:00 +0200

    Lab1(C#)
    Lab1 on C#

---
+++ Lab1.1/MyString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyStringNamespace{
+    class MyString{
+        private string str;
+        public MyString(string s){
+            str = s;
+        }
+        public bool Check(){
+            foreach (char c in str){
+
+                if (!char.IsDigit(c)){
+                    return false;
+                }
+            }
+            return true;
+        }
+        public int Length(){
+            return str.Length;
+        }
+        public void RemoveFive(){
+
+            int pos = str.IndexOf('5');
+            while (pos != -1){
+                str = str.Remove(pos, 1);
+                pos = str.IndexOf('5');
+            }
+        }
+        public int Length2(){
+            return str.Length;
+        }
+        public void Print(){
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Lab1.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyStringNamespace{
+    class Program{
+        static void Main(string[] args){
+
+            bool runAgain = true;
+            while (runAgain){
+
+                Console.Write("Enter a string of numeric values: ");
+                string a = Console.ReadLine();
+                MyString s = new MyString(a);
+
+                if (!s.Check()){
+                    Console.WriteLine("Error: string contains non-digit characters.");
+                    Console.WriteLine();
+                    continue;
+                }
+
+                Console.Write("Original string: ");
+                s.Print();
+
+                Console.WriteLine("Length of string: " + s.Length());
+
+                s.RemoveFive();
+
+                Console.Write("String after removing 5: ");
+                s.Print();
+
+                Console.WriteLine("Length of string after removing 5: " + s.Length2());
+                Console.WriteLine();
+
+                Console.Write("Continue? (Yes/No): ");
+                string answer = Console.ReadLine();
+                Console.WriteLine();
+
+                if (answer.ToLower() == "no"){
+                    runAgain = false;
+                }
+            }
+        }
+    }
+}
